fdd85b4af10d11bf3cf265ca368dd89377c6d7af
aletaffo <alessandrotaffini@gmail.com>
2022-02-10 08:51:55 +0100

    fatto la scrittura

--- app/src/main/java/com/example/file/Gestore.java
+++ app/src/main/java/com/example/file/Gestore.java
@@ -2,9 +2,11 @@ package com.example.file;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.contentcapture.ContentCaptureCondition;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
@@ -56,9 +58,23 @@ public class Gestore {                  //1)stream di byte
 
 
 
-    public void scriviFile(String nomeFile,String testo)
-    {
+    public String scriviFile(String nomeFile, Context c){
+        FileOutputStream fileO;
+        String testodascrivere="testo del file";
+        String esito="";
 
+        try {
+            fileO = c.openFileOutput(nomeFile,Context.MODE_PRIVATE);
+            fileO.write(testodascrivere.getBytes());
+            fileO.close();
+            esito="scrittura corretta";
+        } catch (FileNotFoundException e) { //FNF
+            e.printStackTrace();
+            esito="attenzione errore in apertura";
+        } catch (IOException e) {          //I/O
+            esito="errore in scrittura";
+        }
+        return esito;
     }
 
 }
--- app/src/main/java/com/example/file/MainActivity.java
+++ app/src/main/java/com/example/file/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
      Button btnLeggi;
@@ -30,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String risultaro = gestore.leggiFile("fileDaLeggere.txt",getApplicationContext());
 
+            }
+        });
+        btnScrivi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String esito = gestore.scriviFile("prova.txt",getApplicationContext());
+                Toast.makeText(getApplicationContext(),esito,)
             }
         });
 
